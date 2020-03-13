ab21e3d246bafcb5b0d3c6505a7a27c83ec2497d
jim2 <jacob.monroe@nist.gov>
2020-03-13 09:00:38 -0400

    Added separate training function with custom training loop to provide more control of optimizer and how losses are calculated.

--- train.py
+++ train.py
@@ -128,3 +128,105 @@ def train(model,
   model.save(os.path.join(save_dir, 'model'), save_format='tf')
 
 
+def trainCustom(model,
+                data_file,
+                num_epochs=2,
+                batch_size=64,
+                save_dir='vae_info',
+                overwrite=False):
+  """Trains a VAE model and saves the results in a way that the model can be fully reloaded.
+Uses a custom training loop rather than those built into the tf.keras.Model class.
+
+  Args:
+    model: the VAE model object to train and save
+    data_file: a file containing the data for training/validation
+    num_epochs: Integer with number of epochs to train (each is over all samples)
+    batch_size: Integer with the batch size
+    save_dir: String with path to directory to save to
+    overwrite: Boolean determining whether data is overwritten
+  """
+
+  #Check if the directory exists
+  #If so, assume continuation and load model weights
+  if os.path.isdir(save_dir):
+    #If overwrite is True, don't need to do anything
+    #If it's False, create a new directory to save to
+    if not overwrite:
+      print("Found saved model at %s and overwrite is False."%save_dir)
+      print("Will attempt to load and continue training.")
+      model.load_weights(os.path.join(save_dir, 'training.ckpt'))
+      #print(model.summary())
+      try:
+        dir_split = save_dir.split('_')
+        train_num = int(dir_split[-1])
+        save_dir = '%s_%i'%("_".join(dir_split[:-1]), train_num+1)
+      except ValueError:
+        save_dir = '%s_1'%(save_dir)
+      os.mkdir(save_dir)
+  #If not, create that directory to save to later
+  else:
+    os.mkdir(save_dir)
+
+  print("Model set up and ready to train.")
+
+  #Want to load in data
+  #Can actually just give numpy arrays to the Model.fit function in tf.keras
+  #So could just load .nc files directly
+  #Would still like to provide a wrapper in dataloaders.py
+  #Will make more generalizable in case data format changes
+  #But, something weird with batching happens if you use keras loss functions
+  #trainData, valData = dataloaders.image_data(data_file, batch_size, val_frac=0.1)
+  #trainData = dataloaders.raw_image_data(data_file)
+  trainData, valData = dataloaders.dsprites_data(batch_size, val_frac=0.01)
+
+  #Set up path for checkpoint files
+  checkpoint_path = os.path.join(save_dir, 'training.ckpt')
+
+  #Create an optimizer to use based on hyperparameters in https://github.com/google-research/disentanglement_lib 
+  optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001,
+                                       beta_1=0.9,
+                                       beta_2=0.999,
+                                       epsilon=1e-08,
+                                      )
+
+  #Specify the loss function we want to use
+  #loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=True,
+  #                                reduction=tf.keras.losses.Reduction.SUM),
+  loss_fn = losses.ReconLoss()
+
+  print("Beginning training at: %s"%time.ctime())
+
+  #Loop over epochs
+  for epoch in range(num_epochs):
+    print('\nOn epoch %d:'%epoch)
+
+    #Iterate over batches in the dataset
+    for step, x_batch_train in enumerate(trainData):
+      with tf.GradientTape() as tape:
+        reconstructed = model(x_batch_train[0])
+        loss = loss_fn(x_batch_train, reconstructed)
+        loss += sum(model.losses)
+
+      grads = tape.gradient(loss, model.trainable_weights)
+      optimizer.apply_gradients(zip(grads, model.trainable_weights))
+
+      if step%100 == 0:
+        print('\tStep %i: loss=%f, kl_div=%f, reg_loss=%f'
+              %(step, loss, model.metrics[0], model.metrics[1]))
+
+    #Save checkpoint after each epoch
+    print('\tEpoch finished, saving checkpoint.')
+    model.save_weights(checkpoint_path.format(epoch=epoch))
+
+  print("Training completed at: %s"%time.ctime())
+  print(model.summary())
+
+  #print(train_history.history)
+
+  #At the end, save the full model
+  #This is of limited usefulness due to difficulties saving and loading custom models
+  #If can manage not to have any custom losses or metrics, loads just fine!
+  #Well, fine except that it may lose the regularizer function
+  model.save(os.path.join(save_dir, 'model'), save_format='tf')
+
+
